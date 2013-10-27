ffbf5ba15c35f7d2cba6a4c2ab6a31c9b3003be1
Shiki <shikishiji@gmail.com>
2013-10-27 12:06:49 +0800

    Initial SKStatefulTableViewController from separate project. Removed tests target from the demo project.

---
+++ SKStatefulTableViewController/SKStatefulTableViewController.h
@@ -0,0 +1,58 @@
+//
+//  SKStatefulTableViewController
+//  SKStatefulTableViewController
+//
+//  Created by Shiki on 10/24/13.
+//  Copyright (c) 2013 Shiki. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+  SKStatefulTableViewControllerStateIdle = 0,
+  SKStatefulTableViewControllerStateInitialLoading,
+  SKStatefulTableViewControllerStateEmptyOrInitialLoadError,
+  SKStatefulTableViewControllerStateLoadingFromPullToRefresh,
+  SKStatefulTableViewControllerStateLoadingMore
+} SKStatefulTableViewControllerState;
+
+@class SKStatefulTableViewController;
+
+@protocol SKStatefulTableViewControllerDelegate <NSObject>
+
+- (void)statefulTableViewWillBeginInitialLoad:(SKStatefulTableViewController *)tableView
+                                   completion:(void (^)(BOOL tableIsEmpty, NSError *errorOrNil))completion;
+- (void)statefulTableViewWillBeginLoadingFromPullToRefresh:(SKStatefulTableViewController *)tableView
+                                                completion:(void (^)(BOOL tableIsEmpty, NSError *errorOrNil))completion;
+- (void)statefulTableViewWillBeginLoadingMore:(SKStatefulTableViewController *)tableView
+                                   completion:(void (^)(BOOL canLoadMore, NSError *errorOrNil))completion;
+
+@optional
+
+- (UIView *)statefulTableViewViewForInitialLoad:(SKStatefulTableViewController *)tableView;
+// TODO rename since this is reused for pull to refresh as well
+- (UIView *)statefulTableView:(SKStatefulTableViewController *)tableView viewForEmptyInitialLoadWithError:(NSError *)errorOrNil;
+- (UIView *)statefulTableView:(SKStatefulTableViewController *)tableView viewForLoadMoreWithError:(NSError *)errorOrNil;
+
+@end
+
+@interface SKStatefulTableViewController : UIViewController
+
+@property (weak, nonatomic) id<SKStatefulTableViewControllerDelegate> delegate;
+
+@property (readonly, strong, nonatomic) UITableView *tableView;
+@property (readonly, strong, nonatomic) UIView *staticContainerView;
+
+@property (nonatomic) SKStatefulTableViewControllerState state;
+
+@property (readonly, nonatomic) BOOL hasFinishedInitialLoad;
+
+- (void)triggerInitialLoad;
+- (void)setHasFinishedInitialLoad:(BOOL)tableIsEmpty withError:(NSError *)errorOrNil;
+- (void)triggerPullToRefresh;
+- (void)setHasFinishedLoadingFromPullToRefresh:(BOOL)tableIsEmpty withError:(NSError *)errorOrNil;
+- (void)triggerLoadMore;
+- (void)setHasFinishedLoadingMore:(BOOL)canLoadMore withError:(NSError *)errorOrNil;
+
+@end
---
+++ SKStatefulTableViewController/SKStatefulTableViewController.m
@@ -0,0 +1,203 @@
+//
+//  SKStatefulTableViewController
+//  SKStatefulTableViewController
+//
+//  Created by Shiki on 10/24/13.
+//  Copyright (c) 2013 Shiki. All rights reserved.
+//
+
+
+#import "SKStatefulTableViewController.h"
+
+typedef enum {
+  SKStatefulTableViewControllerViewModeStatic = 1,
+  SKStatefulTableViewControllerViewModeTable,
+} SKStatefulTableViewControllerViewMode;
+
+@interface SKStatefulTableViewController ()
+
+@property (readwrite, strong, nonatomic) UITableView *tableView;
+@property (readwrite, strong, nonatomic) UIView *staticContainerView;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
+
+@end
+
+@implementation SKStatefulTableViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+  tableView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight;
+  [self.view addSubview:tableView];
+  self.tableView = tableView;
+
+  // Add UIRefreshControl without the need for self to be a UITableViewController.
+  // http://stackoverflow.com/questions/12497940/uirefreshcontrol-without-uitableviewcontroller
+  UITableViewController *tableViewController = [[UITableViewController alloc] init];
+  tableViewController.tableView = tableView;
+  UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+  [refreshControl addTarget:self action:@selector(refreshControlValueChanged:)
+           forControlEvents:UIControlEventValueChanged];
+  tableViewController.refreshControl = refreshControl;
+  self.refreshControl = refreshControl;
+
+  UIView *staticContentView = [[UIView alloc] initWithFrame:self.view.bounds];
+  staticContentView.backgroundColor = [UIColor whiteColor];
+//  staticContentView.backgroundColor = UIColor.redColor;
+  staticContentView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight;
+  [tableView addSubview:staticContentView];
+//  [self.view addSubview:staticContentView];
+  self.staticContainerView = staticContentView;
+
+  [self triggerInitialLoad];
+}
+
+- (void)refreshControlValueChanged:(id)sender {
+  [self triggerPullToRefresh];
+}
+
+- (void)triggerInitialLoad {
+  if ([self stateIsLoading])
+    return;
+
+  [self setState:SKStatefulTableViewControllerStateInitialLoading];
+
+  __weak typeof(self) wSelf = self;
+  [self.delegate statefulTableViewWillBeginInitialLoad:self completion:^(BOOL tableIsEmpty, NSError *errorOrNil) {
+    [wSelf setHasFinishedInitialLoad:tableIsEmpty withError:errorOrNil];
+  }];
+  UIView *initialLoadView = [self viewForInitialLoad];
+  [self resetStaticContentViewWithChildView:initialLoadView];
+
+  [self setViewMode:SKStatefulTableViewControllerViewModeStatic];
+}
+
+- (void)setHasFinishedInitialLoad:(BOOL)tableIsEmpty withError:(NSError *)errorOrNil {
+  if (self.state != SKStatefulTableViewControllerStateInitialLoading)
+    return;
+
+  if (errorOrNil || tableIsEmpty) {
+    UIView *view = [self viewForEmptyInitialLoadWithError:errorOrNil];
+    [self resetStaticContentViewWithChildView:view];
+    [self setState:SKStatefulTableViewControllerStateEmptyOrInitialLoadError];
+  } else {
+    [self setState:SKStatefulTableViewControllerStateIdle];
+    [self setViewMode:SKStatefulTableViewControllerViewModeTable];
+  }
+}
+
+- (void)triggerPullToRefresh {
+  if ([self stateIsLoading])
+    return;
+
+  [self setState:SKStatefulTableViewControllerStateLoadingFromPullToRefresh];
+
+  __weak typeof(self) wSelf = self;
+  [self.delegate statefulTableViewWillBeginLoadingFromPullToRefresh:self completion:^(BOOL tableIsEmpty, NSError *errorOrNil) {
+    [wSelf setHasFinishedLoadingFromPullToRefresh:tableIsEmpty withError:errorOrNil];
+  }];
+
+  [self.refreshControl beginRefreshing];
+}
+
+- (void)setHasFinishedLoadingFromPullToRefresh:(BOOL)tableIsEmpty withError:(NSError *)errorOrNil {
+  if (self.state != SKStatefulTableViewControllerStateLoadingFromPullToRefresh)
+    return;
+
+  [self.refreshControl endRefreshing];
+
+  if (errorOrNil || tableIsEmpty) {
+    UIView *view = [self viewForEmptyInitialLoadWithError:errorOrNil];
+    [self resetStaticContentViewWithChildView:view];
+    [self setState:SKStatefulTableViewControllerStateEmptyOrInitialLoadError];
+    [self setViewMode:SKStatefulTableViewControllerViewModeStatic];
+  } else {
+    [self setState:SKStatefulTableViewControllerStateIdle];
+    [self setViewMode:SKStatefulTableViewControllerViewModeTable];
+  }
+}
+
+- (UIView *)viewForInitialLoad {
+  if ([self.delegate respondsToSelector:@selector(statefulTableViewViewForInitialLoad:)]) {
+    return [self.delegate statefulTableViewViewForInitialLoad:self];
+  } else {
+    UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc]
+      initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    activityIndicatorView.frame = ({
+      CGRect f = activityIndicatorView.frame;
+      f.origin.x = self.staticContainerView.frame.size.width * 0.5f - f.size.width * 0.5f;
+      f.origin.y = self.staticContainerView.frame.size.height * 0.5f - f.size.height * 0.5f;
+      f;
+    });
+    [activityIndicatorView startAnimating];
+    return activityIndicatorView;
+  }
+}
+
+- (UIView *)viewForEmptyInitialLoadWithError:(NSError *)errorOrNil {
+  if ([self.delegate respondsToSelector:@selector(statefulTableView:viewForEmptyInitialLoadWithError:)]) {
+    return [self.delegate statefulTableView:self viewForEmptyInitialLoadWithError:errorOrNil];
+  } else {
+    UIView *container = [[UIView alloc] initWithFrame:({
+      CGRect f = CGRectMake(0.f, 0.f, self.staticContainerView.bounds.size.width, 120.f);
+      f.origin.y = self.staticContainerView.bounds.size.height * 0.5f - f.size.height * 0.5f;
+      f;
+    })];
+
+    UILabel *label = [[UILabel alloc] init];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = errorOrNil ? errorOrNil.localizedDescription : @"No records found.";
+    [label sizeToFit];
+    label.frame = ({
+      CGRect f = label.frame;
+      f.origin.x = container.bounds.size.width * 0.5f - label.bounds.size.width * 0.5f;
+      f;
+    });
+
+    [container addSubview:label];
+    if (errorOrNil) {
+      UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+      [button setTitle:@"Try Again" forState:UIControlStateNormal];
+      [button addTarget:self action:@selector(triggerInitialLoad) forControlEvents:UIControlEventTouchUpInside];
+      button.frame = ({
+        CGRect f = CGRectMake(0.f, 0.f, 130.f, 32.f);
+        f.origin.x = container.bounds.size.width * 0.5f - f.size.width * 0.5f;
+        f.origin.y = label.frame.origin.y + label.frame.size.height + 10.f;
+        f;
+      });
+      [container addSubview:button];
+    }
+
+    return container;
+  }
+}
+
+- (void)setViewMode:(SKStatefulTableViewControllerViewMode)mode {
+  self.staticContainerView.hidden = mode == SKStatefulTableViewControllerViewModeTable;
+  if (!self.staticContainerView.hidden) {
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+  }
+//  UIView *toShow = mode == SKStatefulTableViewControllerViewModeStatic ? self.staticContainerView : self.tableView;
+//  UIView *toHide = mode == SKStatefulTableViewControllerViewModeStatic ? self.tableView : self.staticContainerView;
+//
+//  if (toHide.hidden && !toShow.hidden) // Nothing to do
+//    return;
+//
+//  // TODO animate?
+//  toHide.hidden = YES;
+//  toShow.hidden = NO;
+}
+
+- (void)resetStaticContentViewWithChildView:(UIView *)childView {
+  [self.staticContainerView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+  [self.staticContainerView addSubview:childView];
+}
+
+- (BOOL)stateIsLoading {
+  return self.state == SKStatefulTableViewControllerStateInitialLoading
+    | self.state == SKStatefulTableViewControllerStateLoadingFromPullToRefresh
+    | self.state == SKStatefulTableViewControllerStateLoadingMore;
+}
+
+@end
